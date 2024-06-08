0f39476f281acbcfb32fbae003bc3ba676820182
Rodrigo Neiland <rodrigo.naohan@gmail.com>
2024-06-08 09:59:35 -0400

    update0806202409:59

--- src/screens/HomeInicial/Home.tsx
+++ src/screens/HomeInicial/Home.tsx
@@ -1,6 +1,5 @@
 import { SafeAreaView, View, TouchableOpacity, Text, Alert } from "react-native";
 import { styles } from "./StyleHome";
-import { useAuth } from "../../contexts/auth";
 import { useEffect, useRef, useState } from "react";
 import MapView, { Marker, Callout } from "react-native-maps";
 import { getCurrentPositionAsync, LocationAccuracy, LocationObject, requestForegroundPermissionsAsync, watchPositionAsync } from "expo-location";
@@ -8,13 +7,9 @@ import { markers } from "./markers";
 import DelegaciaCICOM from "../../components/Home/DelegaciaCICOM";
 import DelegaciaDIP from "../../components/Home/DelegaciaDIP";
 import Microfone from "../../components/Home/microfone";
+import IconDEL from "../../components/Home/iconDEL";
 
 export default function HomeInicial({ navigation }: any) {
-    const { signOut, user } = useAuth()
-
-    const handleSignOut = () => {
-        signOut()
-    };
 
     const [location, setLocation] = useState<LocationObject | null>(null)
 
@@ -56,8 +51,6 @@ export default function HomeInicial({ navigation }: any) {
         <SafeAreaView style={styles.geral}>
             <View style={styles.map}>
 
-
-
                 {location && (
                     <MapView
                         ref={mapRef}
@@ -79,7 +72,7 @@ export default function HomeInicial({ navigation }: any) {
 
                         {CICOM && markers.filter(marker => marker.type === 'CICOM').map((marker, index) => (
                             <Marker key={index} coordinate={marker}>
-                                <Callout>
+                                <Callout tooltip>
                                     <View style={styles.ViewDelegacaia}>
                                         <Text style={styles.TextDelegacia}>{marker.name}</Text>
                                     </View>
@@ -89,7 +82,8 @@ export default function HomeInicial({ navigation }: any) {
 
                         {DIP && markers.filter(marker => marker.type === 'DIP').map((marker, index) => (
                             <Marker key={index} coordinate={marker}>
-                                <Callout>
+                                <IconDEL />
+                                <Callout tooltip>
                                     <View style={styles.ViewDelegacaia}>
                                         <Text style={styles.TextDelegacia}>{marker.name}</Text>
                                     </View>
@@ -99,34 +93,24 @@ export default function HomeInicial({ navigation }: any) {
                     </MapView>
                 )}
 
-
                 <View style={styles.botoesMenu}>
-
-
-
                     <View style={styles.BottomView}>
                         <TouchableOpacity onPress={() => setCICOM(!CICOM)} >
-                            {
-                                CICOM ? <DelegaciaCICOM /> : <DelegaciaCICOM />
-                            }
+                            { CICOM ? <DelegaciaCICOM /> : <DelegaciaCICOM /> }
                         </TouchableOpacity>
                         <Text style={styles.MarginText}>CICOM</Text>
                     </View>
-                    
+
                     <View style={styles.BottomMicrofone}>
-                        <Microfone></Microfone>
+                        <Microfone />
                     </View>
                     <View style={styles.BottomView}>
                         <TouchableOpacity onPress={() => setDIP(!DIP)} >
-                            {
-                                DIP ? <DelegaciaDIP /> : <DelegaciaDIP />
-                            }
+                            { DIP ? <DelegaciaDIP /> : <DelegaciaDIP /> }
                         </TouchableOpacity>
                         <Text style={styles.MarginText}>DIP</Text>
-
                     </View>
                 </View>
-
 
             </View>
 
--- src/screens/HomeInicial/StyleHome.ts
+++ src/screens/HomeInicial/StyleHome.ts
@@ -2,197 +2,15 @@ import { StyleSheet, Dimensions } from "react-native";
 
 const { width, height } = Dimensions.get("window");
 
-const dezdivididopor36 = 5 / 18
-
 export const styles = StyleSheet.create({
-
-
   geral: {
-    flex: 1
-  },
-
-  container: {
     flex: 1,
-    justifyContent: "center",
-    alignItems: "center",
-    position: "absolute",
-    left: 0,
-    right: 0,
-    top: 0,
-    height: 360,
-    backgroundColor: '#142944',
   },
-
-
-  oros: {
-    color: "#FFFFFF",
-    fontSize: 32,
-  },
-
-  bemVindoAoOros: {
-    width: '100%',
-    height: '45%',
-    alignItems: "center",
-    position: "absolute",
-
-  },
-
-  view: {
-    width: "80%",
-    height: height * 0.44,
-    backgroundColor: "white",
-    alignItems: "center",
-    elevation: 4,
-    borderRadius: 12,
-
-  },
-
-  login: {
-    fontSize: width * 0.07,
-    color: "#1B2F49",
-    marginTop: height * 0.04,
-    marginBottom: height * 0.02,
-    textDecorationLine: 'underline',
-    fontFamily: 'MBFSpace',
-    // fontWeight: 'bold'
-
-  },
-
-  EmailSenha: {
-    fontFamily: 'GothamLight',
-    color: "#808080",
-    alignSelf: 'flex-start',
-    marginLeft: width * 0.1,
-    marginTop: width * 0.03,
-    fontSize: height * 0.02,
-  },
-
-  GmailComImagem: {
-    flexDirection: 'row',
-    marginRight: '-4%',
-    alignItems: 'center',
-    width: "80%"
-  },
-
-  SenhaComImagem: {
-    width: "90%",
-    marginRight: '10%',
-    flexDirection: 'row',
-    justifyContent: 'space-between'
-
-  },
-
-  InputComCadeado: {
-    flexDirection: 'row',
-    marginLeft: '14%',
-
-  },
-
-  buttonLogin: {
-    backgroundColor: "#003470",
-    color: "white",
-    width: 135,
-    height: 35,
-    borderRadius: 20,
-    elevation: 12,
-    fontSize: 20,
-    textAlign: "center",
-    marginTop: "5%",
-    fontWeight: 'bold',
-
-  },
-
-  Button: {
-    alignItems: 'center',
-    textAlign: "center",
-    justifyContent: 'center',
-
-  },
-
-  Input: {
-    alignSelf: "flex-start",
-    marginLeft: width * 0.01,
-    width: "80%",
-    fontFamily: 'GothamLight',
-  },
-
-  EsqueceuSuaSenha: {
-    marginLeft: "40%",
-    color: "#BA0808",
-    textDecorationLine: 'underline',
-    marginTop: width * 0.02,
-    justifyContent: 'flex-end',
-    fontFamily: 'GothamLight',
-
-  },
-
-  NaoPossuiUmaContaAindaCadastrese: {
-    flexDirection: 'row',
-  },
-
-  NaoPossuiUmaContaAinda: {
-    fontSize: height * 0.015,
-    marginTop: width * 0.02,
-    color: "#808080",
-    fontFamily: 'GothamLight',
-
-  },
-
-  Cadastrase: {
-    fontSize: height * 0.015,
-    marginTop: width * 0.02,
-    color: "#0C4EA3",
-    textDecorationLine: 'underline',
-    fontWeight: 'bold',
-
-
-  },
-
-  EmergenciaContate: {
-    color: "#142944",
-    fontWeight: 'bold',
-    fontStyle: 'italic',
-    marginTop: width * 0.07,
-    marginBottom: width * 0.04
-
-  },
-
-  ViewEmergence: {
-    flexDirection: 'row',
-    justifyContent: 'space-evenly',
-    width: '100%'
-  },
-
-  EmergenciaContateBotoes: {
-    alignItems: 'center',
-  },
-
-  BombeiroPoliciAmbulancia: {
-    color: "#142944",
-    fontWeight: 'bold',
-    fontStyle: 'italic',
-  },
-
-  errorText: {
-    color: '#BA0808',
-    fontSize: 14,
-    fontFamily: 'GothamLight',
-
-  },
-
-  containermap: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center'
-  },
-
   map: {
     flex: 1,
     width: '100%',
-    height: height
+    height: height,
   },
-
   botoesMenu: {
     alignItems: 'center',
     justifyContent: 'space-around',
@@ -205,25 +23,31 @@ export const styles = StyleSheet.create({
     borderTopLeftRadius: 25,
     borderTopRightRadius: 25,
   },
-
   BottomView: {
-    alignItems: 'center'
+    alignItems: 'center',
   },
-
   MarginText: {
     fontFamily: 'GothamLight',
-    fontWeight: '600'
-
+    fontWeight: '600',
   },
-
   ViewDelegacaia: {
-    padding: 10
+    padding: 10,
+    backgroundColor: '#D9D9D9', 
+    borderRadius: 20, 
+    shadowColor: '#000',
+    shadowOffset: { width: 0, height: 2 },
+    shadowOpacity: 0.8,
+    shadowRadius: 2,
+    elevation: 5,
+    width: 150,
+    alignItems: 'center', 
   },
-
   TextDelegacia: {
-    fontSize: 10,
+    fontSize: 12,
+    fontWeight: 'bold',
+    color: 'black',
+    textAlign: 'center', 
   },
-
   BottomMicrofone: {
     height: height * 0.125,
     width: 100,
@@ -234,7 +58,5 @@ export const styles = StyleSheet.create({
     justifyContent: 'center',
     marginBottom: '25%',
     borderRadius: 500,
-  }
+  },
 });
-
-
